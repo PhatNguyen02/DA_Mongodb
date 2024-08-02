2390e7ff70675e9f2ae0b6512325d3d2ee6f4720
phamhuynhhuuthanh <2001210048@hufi.edu.vn>
2024-08-02 21:28:09 +0700

    create user

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const app = express();
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const cors = require('cors');
-
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 // const authJwt = require('./helpers/jwt');
 require('dotenv').config();
 
@@ -22,17 +23,50 @@ const api = process.env.API_URL;
 app.use(morgan('tiny'));
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(cors());
 
+
+// Session setup
+// Create a new instance of MongoStore
+const mongoStore = new MongoStore({
+    mongoUrl: process.env.CONNECTION_STRING, 
+    collectionName: 'sessions'
+  });
+  
+  // Use session middleware
+  app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: mongoStore,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24
+      }
+  }));
+// app.use(session({
+//   secret: process.env.SESSION_SECRET,
+//   resave: false,
+//   saveUninitialized: false,
+//   store: MongoStore.create({
+//     mongoUrl: process.env.CONNECTION_STRING,
+//     collectionName: 'sessions'
+//   }),
+//   cookie: {
+//     maxAge: 1000 * 60 * 60 * 24 // 1 day
+//   }
+// }));
 
 
 // Routers
 const productsRouter = require('./routes/products');
+const authRouter = require('./routes/auth');
 // const categoriesRouter = require('./routers/categories');
 // const userRouter = require('./routers/users');
 
 app.use(`${api}/products` , productsRouter);
+app.use(`${api}/auth` , authRouter);
 // app.use(`${api}/categories` , categoriesRouter);
-// app.use(`${api}/users` , userRouter);
+//app.use(`${api}/users` , userRouter);
 
 
 
